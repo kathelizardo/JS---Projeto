fa17a426dc2f88b02ea4d9ced4d7407003a2ba7c
Kathe Lizardo <lizardokm@gmail.com>
2021-05-15 00:08:39 -0300

    chave e lixo final

--- scripPorf.js
+++ scripPorf.js
@@ -11,41 +11,7 @@
 
 // })
 
-// function createCardElement(title, description, image_url, image_description, copyright) {
-//     let card_el = document.createElement("article")
-//     card_el.classList.add("card", "mb-2", "mx-4", "ms-md-0", "me-md-4")
-  
-//     let image_el = createImageElement(image_url, image_description)
-//     image_el.classList.add("card-img-top")
-  
-//     let card_body_el = document.createElement("div")
-//     card_body_el.classList.add("card-body")
-  
-//     let title_el = document.createElement("h3")
-//     title_el.textContent = title
-//     title_el.classList.add("card-title")
-  
-//     let text_el = document.createElement("p")
-//     text_el.textContent = truncateText(description, 100)
-//     text_el.classList.add("card-text")
-  
-//     let card_footer_el = document.createElement("div")
-//     card_footer_el.classList.add("card-footer")
-  
-//     card_body_el.appendChild(title_el)
-//     card_body_el.appendChild(text_el)
-  
-//     card_el.appendChild(image_el)
-//     card_el.appendChild(card_body_el)
-  
-//     if (copyright) {
-//       card_footer_el.innerHTML = '&copy; '
-//       card_footer_el.textContent += copyright
-//       card_el.appendChild(card_footer_el)
-//     }
-  
-//     return card_el
-//   }
+
 
 
 let request = new XMLHttpRequest()
