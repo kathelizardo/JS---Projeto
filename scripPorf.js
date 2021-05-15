4fe4b4b90775d302192779afc2052ce84f8e663a
Kathe Lizardo <lizardokm@gmail.com>
2021-05-14 22:19:20 -0300

    repositorio pronto

---
+++ scripPorf.js
@@ -0,0 +1,21 @@
+let request = new XMLHttpRequest()
+request.open('GET', 'https://api.github.com/users/kathelizardo/repos', true)
+
+request.onload = function () {
+  var data = JSON.parse(this.response);
+
+  var statusHTML = '';
+  $.each(data, function(i, status) {
+    statusHTML += '<tr>';
+    statusHTML += '<td>' + status.id + '</td>';
+    statusHTML += '<td>' + status.name + '</td>';
+    statusHTML += '<td>' + status.html_url + '</td>';
+    statusHTML += '<td>' + status.language + '</td>';
+    statusHTML += '</tr>';
+  });
+  $('tbody').html(statusHTML);
+}
+request.send();
+
+
+
